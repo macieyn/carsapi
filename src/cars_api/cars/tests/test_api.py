606abf4b503e62c336e4e9743e1302c7d7c8b579
macieyn <maciej.nachtygal@gmail.com>
2021-03-17 21:28:41 +0100

    Cars Api Tests: post request passing

--- src/cars_api/cars/tests/test_api.py
+++ src/cars_api/cars/tests/test_api.py
@@ -1,9 +1,10 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 from cars.models import Car
 
 
-class CarsApiTests(TestCase):
+class CarsApiTests(APITestCase):
 
     def setUp(self, *args, **kwargs):
         Car.objects.create(make="Volkswagen", model="Golf")
@@ -11,6 +12,15 @@ class CarsApiTests(TestCase):
 
     def test_get_cars(self):
         response = self.client.get('/cars/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.json()
         self.assertEqual(len(data), 2)
+
+    def test_post_cars(self):
+        response = self.client.post('/cars/', {'make': 'Volkswagen', 'model': 'Polo'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        data = response.json()
+        self.assertEqual(type(data), dict)
+        self.assertEqual(Car.objects.count(), 3)
+
         
