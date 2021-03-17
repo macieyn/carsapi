9a49aec4253f508ff5240aa6fced6c26a13f0b4b
macieyn <maciej.nachtygal@gmail.com>
2021-03-17 20:26:21 +0100

    Car model: test create Car model passing

--- src/cars_api/cars/models.py
+++ src/cars_api/cars/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
--- src/cars_api/cars/tests/test_models.py
+++ src/cars_api/cars/tests/test_models.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
 
+from cars.models import Car
+
 
 class CarsTests(TestCase):
 
     def test_create_car(self):
         Car.objects.create(make="Volkswagen", model="Golf")
         self.assertEqual(Car.objects.count(), 1)
+
