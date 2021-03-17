92ef5f2dec7d2909fab6b97f0f5fa7678908693c
macieyn <maciej.nachtygal@gmail.com>
2021-03-17 20:04:12 +0100

    settings.py: add rest framework and cars app to installed apps

--- src/cars_api/cars_api/settings.py
+++ src/cars_api/cars_api/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party 
+    'rest_framework',
+
+    # Django apps
+    'cars.apps.CarsConfig',
 ]
 
 MIDDLEWARE = [
