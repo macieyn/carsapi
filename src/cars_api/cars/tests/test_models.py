f00ad8257d2da0e386c0fa20c4afb3cafa828f0d
macieyn <maciej.nachtygal@gmail.com>
2021-03-17 20:31:26 +0100

    Rate model: add test

--- src/cars_api/cars/tests/test_models.py
+++ src/cars_api/cars/tests/test_models.py
@@ -9,3 +9,14 @@ class CarsTests(TestCase):
         Car.objects.create(make="Volkswagen", model="Golf")
         self.assertEqual(Car.objects.count(), 1)
 
+
+class RateTests(TestCase):
+
+    def setUp(self, *args, **kwargs):
+        Car.objects.create(make="Volkswagen", model="Golf")
+        return super().setUp(*args, **kwargs)
+
+    def test_create_car_rate(self):
+        car = Car.objects.get(id=1)
+        Rate.objects.create(rating=5, car=car)
+        self.assertEqual(Rate.objects.count(), 1)
