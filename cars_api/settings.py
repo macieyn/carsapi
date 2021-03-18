bcf9ca5441a26846cba7b4799cdc806c5976261f
macieyn <maciej.nachtygal@gmail.com>
2021-03-18 22:43:09 +0100

    trying to figure out heroku and postgres 2

--- cars_api/settings.py
+++ cars_api/settings.py
@@ -78,16 +78,16 @@ WSGI_APPLICATION = "cars_api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
 if os.getenv('HEROKU'):
     import dj_database_url
     DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
 
 
 # Password validation
