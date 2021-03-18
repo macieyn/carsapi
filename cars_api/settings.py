6d30902d6b0c5a31d3b1572d0f001a1eb3743e08
macieyn <maciej.nachtygal@gmail.com>
2021-03-18 23:43:53 +0100

    fix debug flag env

--- cars_api/settings.py
+++ cars_api/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.getenv('DEBUG'))
+DEBUG = False if os.getenv('DEBUG') == '0' else True
 
 ALLOWED_HOSTS = ["*"]
 
