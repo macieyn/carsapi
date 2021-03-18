06311498104bfe20446b13db7bc27663d009594d
macieyn <maciej.nachtygal@gmail.com>
2021-03-18 23:35:03 +0100

    move to postgres db

--- cars_api/settings.py
+++ cars_api/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,17 +81,10 @@ WSGI_APPLICATION = "cars_api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(conn_max_age=600)
 }
-
-if os.getenv("HEROKU"):
-    import dj_database_url
-
-    DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
